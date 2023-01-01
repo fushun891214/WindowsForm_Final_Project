9698c5d3bd93b665a83502d0fa7041f8af00a8d0
fushun zhang <fushun891214@gmail.com>
2023-01-02 00:52:05 +0800

    2023/01/02/00:51

---
+++ final_project/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace final_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(124, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label1.Location = new System.Drawing.Point(99, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "時間：";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label2.Location = new System.Drawing.Point(165, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label3.Location = new System.Drawing.Point(99, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "分數：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label4.Location = new System.Drawing.Point(165, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(335, 425);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "辨色力大挑戰";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ final_project/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_project
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttonList = new List<Button>();
+        List<int> numberList = new List<int>();
+        int x = 0;
+        int y = 0;
+        int score = 0;
+        //String result = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            //label4.Text = score.ToString();
+            x = 1;
+            y = 0;
+            
+            InitButton();
+            //InitList();
+            SuffleListAndColor();
+            button1.Enabled = false;
+            timer1.Start();
+
+            //Console.WriteLine(buttonList[1].Tag.ToString());
+
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                //Console.WriteLine(numberList[i]);
+                //Console.WriteLine(buttonList[i].Tag.ToString());
+            }
+
+            //Random random = new Random();
+            //Console.WriteLine(buttonList.Count);
+            //Console.WriteLine(random.Next(0, buttonList.Count+1));
+        }
+
+        void InitButton()
+        {
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                this.Controls.Remove(buttonList[i]);
+                
+            }
+            buttonList.Clear();
+            int counter = 1;
+            for (int i = 0; i < 6 ; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Button button = new Button();
+                    //button.Name = "button" + Convert.ToString(counter);
+                    //button.Tag = (counter);
+                    counter++;
+                    button.Text = "";
+                    button.TabIndex = 0;
+
+                    int buttonWidth = 50;
+                    int buttonHeight = 50;
+
+                    int buttonSpace = 5;
+
+                    int buttonX = buttonSpace + i * (buttonWidth + buttonSpace);
+                    int buttonY = buttonSpace + (j+1) * (buttonHeight + buttonSpace);
+
+                    button.Size = new Size(buttonWidth, buttonHeight);
+
+                    button.Location = new Point(buttonX, buttonY);
+
+                    //UseVisualStyleBackColor = false才能設定顏色
+                    button.UseVisualStyleBackColor = false;
+
+                    button.Click += new System.EventHandler(this.Button_Click_Playgame);
+
+                    this.Controls.Add(button);
+
+                    this.buttonList.Add(button);
+
+                }
+            }
+            //suffleList();
+        }
+
+        void InitList()
+        {
+            numberList.Clear();
+            for (int i = 1; i <= buttonList.Count; i++)
+            {
+                numberList.Add(i);
+            }
+        }
+
+        void SuffleListAndColor()
+        {
+            InitList();
+            Random random = new Random();
+
+            int x = random.Next(1, 255);
+            int y = random.Next(1, 255);
+            int z = random.Next(1, 255);
+            int random_x = 0;
+            int random_y = 0;
+            int random_z = 0;
+            //initList();
+
+
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                int randomNumber = random.Next(1, buttonList.Count);
+                int temp = numberList[i];
+                numberList[i] = numberList[randomNumber];
+                numberList[randomNumber] = temp;
+            }
+
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                //buttonList[i].Text = "";
+                buttonList[i].Tag = numberList[i];
+                buttonList[i].Name = "button" + Convert.ToString(buttonList[i].Tag);
+                //buttonList[i].Text = Convert.ToString(buttonList[i].Tag);
+
+                buttonList[i].BackColor = System.Drawing.Color.FromArgb(x, y, z);
+
+                if (buttonList[i].Tag.Equals(18))
+                {
+                    if (x < 128) random_x = (x + 20);
+                    if (y < 128) random_y = (y + 20);
+                    if (z < 128) random_z = (z + 20);
+                    if (x > 129) random_x = (x - 20);
+                    if (y > 129) random_y = (y - 20);
+                    if (z > 129) random_z = (z - 20);
+                    //buttonList[buttonList.Count - 1].BackColor = System.Drawing.Color.FromArgb(100, 100, 100);
+                    buttonList[i].BackColor = System.Drawing.Color.FromArgb(random_x, random_y, random_z);
+                }
+            }
+            //Console.WriteLine(random.Next(0, buttonList.Count));
+        }
+
+        void Button_Click_Playgame(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int tag = Convert.ToInt32(button.Tag);
+            
+            if (tag.Equals(18))
+            {
+                score += 10;
+                SuffleListAndColor();
+               // button.BackColor = Color.AliceBlue;
+            }
+            else
+            {
+                score -= 2;
+                //button.BackColor = Color.Black;
+            }
+            label4.Text = score.ToString();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = x.ToString() + "分" + y.ToString() + "秒";
+            
+            if (x == 0 && y == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("遊戲結束囉！");
+                button1.Enabled = true;
+                
+            }
+            else if (y == 0 && x != 0)
+            {
+                x -= 1;
+                y = 59;
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = false;
+                y -= 1;
+            }
+        }
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
